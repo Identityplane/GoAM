8de372c0f4e79a5a62540d11973ac5eead9be8cf
gianlucafrei <gianlucafrei@gmail.com>
2025-07-08 00:23:01 +0800

    Added package Entrypoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"github.com/gianlucafrei/GoAM/internal" // lint:ignore ST1019 (This should be fixed, but is not a priority)
+	// lint:ignore ST1019 (This should be fixed, but is not a priority)
 	"github.com/gianlucafrei/GoAM/internal/logger"
-	"github.com/gianlucafrei/GoAM/internal/web"
+	"github.com/gianlucafrei/GoAM/pkg"
 
 	// lint:ignore ST1019
 
 	"os"
-
-	"github.com/valyala/fasthttp"
 )
 
 func main() {
@@ -18,18 +16,5 @@ func main() {
 	wd, _ := os.Getwd()
 	logger.DebugNoContext("Starting GoIAM 0.0.1 with pwd: %s\n", wd)
 
-	// Init Flows
-	internal.Initialize()
-
-	// Start web adapter
-	startWebAdapter()
-}
-
-// startWebAdapter initializes and starts the web server
-func startWebAdapter() {
-	r := web.New()
-	logger.DebugNoContext("Server running on http://localhost:8080")
-	if err := fasthttp.ListenAndServe(":8080", web.TopLevelMiddleware(r.Handler)); err != nil {
-		logger.PanicNoContext("Error: %s", err)
-	}
+	pkg.Run()
 }
