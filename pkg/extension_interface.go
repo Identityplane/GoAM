0a819256c01c5fe3886a9c4f91dcb9a89005b7a5
gianlucafrei <gianlucafrei@gmail.com>
2025-09-12 14:00:45 +0700

    Added GetServerConfig() to load config by extension

--- pkg/extension_interface.go
+++ pkg/extension_interface.go
@@ -2,7 +2,9 @@ package pkg
 
 import (
 	"github.com/Identityplane/GoAM/internal/auth/graph"
+	"github.com/Identityplane/GoAM/internal/config"
 	"github.com/Identityplane/GoAM/pkg/model"
+	"github.com/Identityplane/GoAM/pkg/server_settings"
 )
 
 // RegisterNode registers a node definition. Use this to add a new custom node
@@ -13,4 +15,9 @@ func RegisterNode(name string, def *model.NodeDefinition) {
 // UnregisterNode unregisters a node definition. Use this to remove a node
 func UnregisterNode(name string) {
 	delete(graph.NodeDefinitions, name)
+}
+
+// GetServerConfig returns a pointer to the current server runtime configuration
+func GetServerConfig() *server_settings.GoamServerSettings {
+	return config.ServerSettings
 }
